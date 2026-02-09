a57973f7c16c90fdae32d932b9ea74b1390ea4d1
Olivier PEREZ <olivier@olivierperez.fr>
2026-02-09 13:17:30 +0100

    fix collision of opened doors

--- objects/door.gd
+++ objects/door.gd
@@ -1,6 +1,15 @@
 extends StaticBody3D
 
 @onready var wall_door_rotate: Node3D = $"wall-door-rotate"
+@onready var closed_shape: CollisionShape3D = $ClosedShape
+@onready var open_shape_1: CollisionShape3D = $OpenShape1
+@onready var open_shape_2: CollisionShape3D = $OpenShape2
 
 func _ready() -> void:
+	open()
+
+func open() -> void:
 	wall_door_rotate.get_node("AnimationPlayer").play("open")
+	closed_shape.disabled = true
+	open_shape_1.disabled = false
+	open_shape_2.disabled = false
